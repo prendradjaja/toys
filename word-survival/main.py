a1da9deed2f427fbad29dbea8f671ca820d0d50f
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-01-13 22:12:12 -0600

    word-survival: Add README

--- word-survival/main.py
+++ word-survival/main.py
@@ -1,36 +1,3 @@
-'''
-1. A simple game: Start with a random rack of Scrabble tiles, e.g.
-
-  SATUDRE
-
-2. Make a word out of them. This is your new rack.
-
-  STARED
-
-3. Remove the first letter and draw a random tile from the (infinite) bag.
-
-  TAREDH
-
-4. Go back to step 2 and repeat until you can no longer make a word, e.g.
-
-  DEARTH -> EARTHX
-  HEART -> EARTP
-  PART -> ARTD
-  DART -> ARTZ
-  TAR -> ARX
-  A -> E
-  (game over)
-
-
-REMARKS:
-
-A small amount of animation in a GUI environment would probably make it more
-obvious what happens at each turn.
-
-This is not a very fun game.
-'''
-
-
 from random_letter import random_letter
 from legal_words import legal_words
 from collections import Counter
