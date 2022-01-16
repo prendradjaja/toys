abcdd67b3ea3db58d609dcbd83394134a4ddf342
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-01-16 09:53:17 -0600

    exponentiation-by-squaring: Rename

--- exponentiation-by-squaring/main.py
+++ exponentiation-by-squaring/main.py
@@ -1,12 +1,12 @@
 '''
 TODO:
-- Fewer multiplications is possible! How? Maybe implement as exp2 below.
+- Fewer multiplications is possible! How?
   https://projecteuler.net/problem=122
 - Use this for AoC 2021.06 "Lanternfish." How fast is it?
   https://adventofcode.com/2021/day/6
 '''
 
-def exp1(k, n):  # Exponentiation by squaring
+def my_exp(k, n):
     # k^n can be decomposed into factors of the form k^(2^p).
     #
     # To see this, consider the binary representation of n. For example, if n
@@ -27,15 +27,11 @@ def exp1(k, n):  # Exponentiation by squaring
 
     binary = bin(n)[2:]
     res = 1
-    for i, bit in enumerate(reversed(binary)):
+    for bit in reversed(binary):
         if bit == '1':
             res *= k
         k = k * k
     return res
-
-
-def exp2(k, n):
-    pass
 
 
 if __name__ == '__main__':
@@ -43,7 +39,7 @@ if __name__ == '__main__':
     for k in range(1, 100):
         for n in range(1, 100):
             expected = k ** n
-            actual = exp1(k, n)
+            actual = my_exp(k, n)
             if expected != actual:
                 print(f'Failed: {k}**{n} should equal {expected}. Actual: {actual}')
                 exit()
