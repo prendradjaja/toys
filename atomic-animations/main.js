08db0a2fe73b907bfc6d439aebdfe164589c5f53
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-07-22 17:01:41 -0500

    atomic-animations: Done!

--- atomic-animations/main.js
+++ atomic-animations/main.js
@@ -1,11 +1,28 @@
+import { Tween, Easing, update } from './tween.esm.js';
+
+
 const $ = s => document.querySelector(s);
 
 let position = { x: 100, y: 100 };
+let activeTween;
 
 function main() {
-  updatePosition();
+  updatePosition(position);
+
+  function animate(time) {
+    requestAnimationFrame(animate);
+    update(time);
+  }
+  requestAnimationFrame(animate);
 
   document.addEventListener('keydown', (e) => {
+    if (activeTween) {
+      activeTween.stop();
+      updatePosition(position);
+    }
+
+    const oldPosition = {...position};
+
     if (e.key === 'j' || e.key === 'ArrowDown') {
       position.y += 100;
     } else if (e.key === 'k' || e.key === 'ArrowUp') {
@@ -15,13 +32,20 @@ function main() {
     } else if (e.key === 'l' || e.key === 'ArrowRight') {
       position.x += 100;
     } else {
-      // Do nothing
+      return;
     }
-    updatePosition();
+
+    activeTween = new Tween({...oldPosition})
+      .to(position, 250)
+      .easing(Easing.Quadratic.Out)
+      .onUpdate((livePosition) => {
+        updatePosition(livePosition);
+      })
+      .start();
   });
 }
 
-function updatePosition() {
+function updatePosition(position) {
   $('#box').style.left = `${position.x}px`;
   $('#box').style.top = `${position.y}px`;
 }
