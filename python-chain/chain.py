b555759e1cc325813ae7bf2f986da5015bc7628d
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2022-09-13 10:50:28 -0500

    python-chain: Add filter_ and sort

--- python-chain/chain.py
+++ python-chain/chain.py
@@ -15,8 +15,24 @@ class map_:
         return [self.fn(x) for x in other]
 
 
+class filter_:
+    def __init__(self, fn):
+        self.fn = fn
+    def __ror__(self, other):
+        return [x for x in other if self.fn(x)]
+
+
 class join:
     def __init__(self, sep):
         self.sep = sep
     def __ror__(self, other):
         return self.sep.join(other)
+
+
+class sort:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+    def __ror__(self, other):
+        other = other[:]
+        other.sort(**self.kwargs)
+        return other
