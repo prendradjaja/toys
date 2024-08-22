ce8e4519a46d19313b76d61e51f9e3cabd802267
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-08-22 13:22:33 -0700

    Use the "pixels" approach

--- seven-segment-display/index.ts
+++ seven-segment-display/index.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 const template = fs.readFileSync('template.txt', 'utf8');
 
-const digitHeight = 3;
+const digitWidth = 3;
 
 const whichSegments: Partial<Record<string, number[]>> = {
   '0': [0, 1, 3, 4, 5, 6],
@@ -17,32 +17,38 @@ const whichSegments: Partial<Record<string, number[]>> = {
   '9': [0, 1, 2, 3, 6],
 };
 
-type DigitImage = string[];
+type Image = ImageChar[];
+interface ImageChar {
+  r: number,
+  c: number,
+  ch: string,
+}
 
 function main() {
   showDigits(1234567890);
 }
 
-function makeDigitImage(n: string): DigitImage {
+function makeDigitImage(n: string): Image {
   const segments = whichSegments[n];
   if (segments === undefined) {
     throw new Error(`Invalid digit: ${n}`);
   }
-  const result: DigitImage = [];
-  let i = 0;
+  const result: Image = [];
+  let r = 0;
+  let c = 0;
   let row = '';
+  let i = 0;
   for (const ch of template) {
     if (ch === '\n') {
-      result.push(row);
-      row = '';
+      r++;
+      c = 0;
     } else if (ch === ' ') {
-      row += ch;
+      c++;
     } else {
       if (segments.includes(i)) {
-        row += ch;
-      } else {
-        row += ' ';
+        result.push({ r, c, ch });
       }
+      c++;
       i++;
     }
   }
@@ -51,14 +57,44 @@ function makeDigitImage(n: string): DigitImage {
 
 function showDigits(n: number): void {
   const digitChars = Array.from(n.toString());
-  const digitImages = digitChars.map(makeDigitImage);
-  for (let i = 0; i < digitHeight; i++) {
+  const fullImage: Image = [];
+  let offset = 0;
+  for (let digitImage of digitChars.map(makeDigitImage)) {
+    digitImage = shift(digitImage, offset);
+    fullImage.push(...digitImage);
+    offset += digitWidth;
+  }
+
+  let rmin = Infinity;
+  let rmax = -Infinity;
+  let cmin = Infinity;
+  let cmax = -Infinity;
+  for (const imageChar of fullImage) {
+    rmin = Math.min(rmin, imageChar.r);
+    rmax = Math.max(rmax, imageChar.r);
+    cmin = Math.min(cmin, imageChar.c);
+    cmax = Math.max(cmax, imageChar.c);
+  }
+
+  for (let r = rmin; r <= rmax; r++) {
     let line = '';
-    for (const image of digitImages) {
-      line += image[i];
+    for (let c = cmin; c <= cmax; c++) {
+      const imageChar = fullImage.find(each => each.r === r && each.c === c);
+      if (imageChar === undefined) {
+        line += ' ';
+      } else {
+        line += imageChar.ch;
+      }
     }
     console.log(line);
   }
+}
+
+function shift(image: Image, horiontalOffset: number) {
+  return image.map(imageChar => ({
+    ...imageChar,
+    c: imageChar.c + horiontalOffset,
+  }));
 }
 
 main();
